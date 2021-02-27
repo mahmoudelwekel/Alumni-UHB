d182cd2001319573cbe5600714568b4722059903
mahmoud elwekel <mahmoudelwekel@ymail.com>
2021-02-27 13:37:12 +0200

    Add Php

---
+++ Code/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <title>Alumni UHB</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1,shrink-to-fit=no" />
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-16" />
+
+
+   
+    <link rel="stylesheet" href="assets/bootstrap.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+    
+    <link rel="stylesheet" href="assets/Custome.CSS" />
+
+
+</head>
+<body>
+
+
+
+header
+<br/>
---
+++ Code/index.php
@@ -0,0 +1,16 @@
+<?php include  "header.php";?>
+
+
+
+
+Body
+<br/>
+
+
+
+
+
+
+
+
+<?php include  "footer.php";?>

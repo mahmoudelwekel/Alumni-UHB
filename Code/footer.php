6d4ea71b292a608a1aa0b0e3480d1bed48b97c10
mahmoud elwekel <mahmoudelwekel@ymail.com>
2021-02-28 00:21:29 +0200

    index design

--- Code/footer.php
+++ Code/footer.php
@@ -1,12 +1,66 @@
+<footer class="bg-secondary py-5">
 
-Footer
-<br/>
+    <div class="container text-white">
+        <div class="row">
+            <div class="col-8 d-flex justify-content-start align-items-center">
+                <div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" integrity="sha256-x3YZWtRjM8bJqf48dFAv/qmgL68SI4jqNWeSLMZaMGA=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha256-WqU1JavFxSAMcLP2WIOI+GB2zWmShMI82mTpLDcqFUg=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Cairo&display=swap" rel="stylesheet" />
+                    <h2 class="border-bottom pb-2">Contact Us</h2>
+
+                    <a href="#" target="_blank" class="text-decoration-none text-white">
+                        <i class="icon fas fa-envelope "></i> Alumni@UHB.EDU.SA
+                    </a>
+                    <br />
+                    <a href="#" target="_blank" class="text-decoration-none text-white">
+                        <i class="icon fas fa-phone-alt "></i> 013-720XXXX
+                    </a>
+                    <br />
+                    <a href="#" id="messenger" target="_blank" class="text-decoration-none text-white">
+                        <i class="icon fab fa-twitter"></i> UHB.OFFCIAL
+                    </a>
+                    <br />
+                    <a href="#" id="insta" target="_blank" class="text-decoration-none text-white">
+                        <i class="icon fab fa-instagram"></i> UHB.OFFCIAL
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 d-flex justify-content-end align-items-center">
+                <img src="Images/logo3.png" class="my-4" />
+
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" integrity="sha256-x3YZWtRjM8bJqf48dFAv/qmgL68SI4jqNWeSLMZaMGA=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha256-WqU1JavFxSAMcLP2WIOI+GB2zWmShMI82mTpLDcqFUg=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Cairo&display=swap" rel="stylesheet" />
 
 </body>
+
 </html>
--- Code/header.php
+++ Code/header.php
@@ -1,26 +1,57 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
 
     <title>Alumni UHB</title>
+    <link rel="icon" href="Images/logo.png" type="image/png" />
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1,shrink-to-fit=no" />
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-16" />
 
 
-   
     <link rel="stylesheet" href="assets/bootstrap.css" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
-    
+
     <link rel="stylesheet" href="assets/Custome.CSS" />
 
 
 </head>
+
 <body>
 
 
+    <div style="background-image: url('Images/header.jpg');background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center; ">
 
-header
-<br/>
+        <div class="container d-flex justify-content-start align-items-center">
+
+
+            <img src="Images/logo.png" class="my-4" />
+        </div>
+
+    </div>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light font-weight-bold">
+        <div class="container">
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Courses</a>
+                    <a class="nav-item nav-link" href="#">WorkShops</a>
+                    <a class="nav-item nav-link" href="#">Jobs</a>
+                    <a class="nav-item nav-link" href="#">Lecturers</a>
+                    <a class="nav-item nav-link" href="#">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>

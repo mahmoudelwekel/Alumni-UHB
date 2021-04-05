b052ea6a0bd9e8fc02cd3a46b29e245549ac9a6b
mahmoud elwekel <mahmoudelwekel@ymail.com>
2021-04-05 23:23:38 +0200

    profile folder updates

--- Code/views/profile/courses.php
+++ Code/views/profile/courses.php
@@ -1,3 +1,91 @@
-<?php require_once"../../init/init.php"; ?>
+<?php require_once "../../init/init.php"; ?>
 
-<?php require_once"../includes/footer.php"; ?>
+<div class="" style="background-image:url('<?= asset("Images/bg/empty.jpg") ?>') ;
+background-repeat: no-repeat;
+    background-size: contain;
+    background-position: right;
+    background-color: #e5f1ed;">
+
+    <div class="container h1 py-5">
+        Courses
+    </div>
+</div>
+
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top p-4" src="<?= asset("Images/logo.png") ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title font-weight-bold  text-primary">Course Title</h5>
+                    <hr />
+                    <p class="card-text text-decoration-none text-secondary">
+                        <i class="icon fas fa-user-tie "></i> Lecturer Name
+                    </p>
+                    <p class="card-text text-decoration-none text-secondary">
+                        <i class="icon fas fa-map-marker-alt "></i> Location
+                    </p>
+                    <p class="card-text text-decoration-none text-secondary">
+                        <i class="icon fas fa-calendar-alt"></i> 12:12 12/12/2021
+                    </p>
+                </div>
+                <a href="#" class="card-footer font-weight-bold">
+                    Certificate >>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <img class="card-img-top p-4" src="<?= asset("Images/certificate.jpg") ?>" alt="Card image cap">
+
+        </div>
+    </div>
+
+
+
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top p-4" src="<?= asset("Images/logo.png") ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title font-weight-bold  text-primary">Course Title</h5>
+                    <hr />
+                    <p class="card-text text-decoration-none text-secondary">
+                        <i class="icon fas fa-user-tie "></i> Lecturer Name
+                    </p>
+                    <p class="card-text text-decoration-none text-secondary">
+                        <i class="icon fas fa-map-marker-alt "></i> Location
+                    </p>
+                    <p class="card-text text-decoration-none text-secondary">
+                        <i class="icon fas fa-calendar-alt"></i> 12:12 12/12/2021
+                    </p>
+                </div>
+                <a href="#" class="card-footer font-weight-bold">
+                    Certificate >>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <img class="card-img-top p-4" src="<?= asset("Images/certificate.jpg") ?>" alt="Card image cap">
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+<?php require_once "../includes/footer.php"; ?>
--- Code/views/profile/update.php
+++ Code/views/profile/update.php
@@ -1,3 +1,41 @@
-<?php require_once"../../init/init.php"; ?>
+<?php require_once "../../init/init.php"; ?>
 
-<?php require_once"../includes/footer.php"; ?>
+<div class="container py-5">
+    <h3>
+        Update Profile
+    </h3>
+    <hr />
+
+    <form>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Name</label>
+            <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Position</label>
+            <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Office</label>
+            <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Age</label>            
+            <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Start Date</label>
+            <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Salary</label>
+            <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <button type="submit" class="btn btn-primary ">Save</button>
+        <a href="index.php" class="btn btn-secondary ml-3">Close</a>
+    </form>
+</div>
+
+<?php require_once "../includes/footer.php"; ?>
